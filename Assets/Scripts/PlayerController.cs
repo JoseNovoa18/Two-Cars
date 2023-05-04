36ef2e0f99d506a3d81eb9255a0bce25456af632
Jose Daniel Novoa Garcia <jdnovoa18@gmail.com>
2023-05-04 02:47:30 -0500

    Create vehicle and movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Hi
+    // Private Variables 
+    private float speed = 25.0f;
+    private float turnSpeed = 25.0f;
+    private float horizontalInput;
+    private float forwardInput;
+
+    private void Start()
+    {
+        
+    }
+    void Update()
+    {
+        // This is where we get player input 
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        // Moves the car forward based on vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+
+        // Rotates the car based on horizontal input
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+    }
+}
